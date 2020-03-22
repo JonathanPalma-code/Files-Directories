db0035e460b0664524dfe062c3caea9aeac71a30
Jonathan <jonathanpalma89@gmail.com>
2020-03-22 16:47:34 +0000

    chore: add Product and Print Class

---
+++ Print.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ManagingFileContent.Entities;
+using System.IO;
+
+namespace ManagingFileContent
+{
+    class Print
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the full path:");
+            string fullpath = Console.ReadLine();
+
+            try
+            {
+                var lines = File.ReadAllLines(fullpath);
+                string sourceFolderPath = Path.GetDirectoryName(fullpath);
+                string targetFolderPath = fullpath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        var fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine($"{product.Name}, {product.TotalPrice()}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
